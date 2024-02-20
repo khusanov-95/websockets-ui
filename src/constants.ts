00f5d50e81962e7e0d6691afe57efe7ace85ef27
khusanov-95 <fa.khusano@gmail.com>
2024-02-20 20:24:36 +0500

    refactor

--- src/constants.ts
+++ src/constants.ts
@@ -2,5 +2,7 @@ export enum messageType {
     reg = "reg",
     createRoom = "create_room",
     addUserToRoom = "add_user_to_room",
-    singlePlay = "single_play"
+    singlePlay = "single_play",
+    updateRoom = "update_room",
+    updateWinners = "update_winners"
 }
--- src/webSocket_server/index.ts
+++ src/webSocket_server/index.ts
@@ -4,59 +4,98 @@ import { messageType } from "../constants";
 import { generateId } from "../helpers";
 
 interface Player {
-  id: number,
   name: string,
-  password: number
+  index: number,
+  id?: number, // remove ?
+  password?: number // remove ?
 }
 
 interface Room {
-  id: number,
-  players: Player[];
+  roomId: number,
+  roomUsers: Player[];
   isAvailable: boolean;
 }
 
-
+const players:Player[] = [];
+const rooms:Room[] = [];
+const tableOfWinners = [] // update/fix
 
 
 export const webSocketServer = () => {
     const webSocketServer = new WebSocketServer({port: 3000}) // replace port ?
-    const players:Player[] = [];
-    const rooms:Room[] = [];
-    const tableOfWinners = [] // update/fix
-    let userId = 1;
+
 
     webSocketServer.on('connection', function connection(ws) {
         ws.on('message', function message(message: string) {
           console.log('received: %s', message);
 
           const {type, data, id} = JSON.parse(message);
+      
+ 
 
           if(type === messageType.reg) {
+            const {name, password} = JSON.parse(data);
 
+            const playerIndex = +players[players.length] || 0;
+            // console.log(players, players.length)
             const player = {
               id: generateId(),
-              name: data.name,
-              password: data.password
+              name,
+              index: playerIndex,
+              password
             }
-
             players.push(player);
 
-            const playerIndex = players[players.length - 1]
 
             ws.send(JSON.stringify({
               type,
-              data: JSON.stringify({name: data.name, index: playerIndex, error: false, errText: 'some error'}),//fix error text
+              data: JSON.stringify({name, index: playerIndex, error: false, errText: 'some error'}),//fix error text
               id
+            }));
+
+            const availableRooms = rooms.find(room => room.isAvailable) || [];
+
+            console.log(availableRooms)
+
+            ws.send(JSON.stringify({
+              type: messageType.updateRoom,
+              data: JSON.stringify(availableRooms),
+              id: 0
+            }));
+
+            ws.send(JSON.stringify({
+              type: messageType.updateWinners,
+              data: JSON.stringify([]), // fix
+              id: 0
             }));
 
           }
 
           if(type === messageType.createRoom) {
-            // rooms.push(['user-one']);
+            // console.log(rooms, rooms.length)
+            const currentPlayer = players[players.length - 1];
 
-          } 
+            rooms.push({
+              roomId: generateId(),
+              roomUsers: [currentPlayer],
+              isAvailable: true
+            });
+
+            const availableRooms = rooms.find(room => room.isAvailable);
+  
+            ws.send(JSON.stringify({
+              type: messageType.updateRoom,
+              data: JSON.stringify(availableRooms),
+              id: 0
+            }));
+            
+          }
 
           if(type === messageType.addUserToRoom) {
+            const availableRoom = rooms.find(room => room.isAvailable);
+            // console.log(availableRoom)
+            const currentPlayer = players[players.length - 1];
+            
             // rooms[0].push('user-two');
             // ws.send(JSON.stringify({
             //   type,
@@ -72,8 +111,6 @@ export const webSocketServer = () => {
           
 
         });
-
-
       
       
       });
